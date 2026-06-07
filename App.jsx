18c2f6d73e40610a21629d1c0c5ac36535272b9b
EvaStilinski <evastilinski@icloud.com>
2026-06-06 18:49:09 -0600

    somethings in app and main jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Hero from './Hero'
+import HoursPanel from './HoursPanel'
+import Footer from './Footer'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <HoursPanel />
+      <Footer />
+    </>
+  )
+}
